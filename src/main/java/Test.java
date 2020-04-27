0f3d1c84a74dec89d13a591c1e67573411ea69c8
zhanghaibo <zhanghaibo@lulumentor>
2020-04-27 20:41:19 +0800

    机试代码

---
+++ src/main/java/Test.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Test {
+
+    private static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+    public static void main(String[] args) {
+        //init map
+        for(int i = 12; i < 20; i ++) {
+            map.put(i, 0);
+        }
+
+        Scanner in = new Scanner(System.in);
+        int num = 0;
+        while (in.hasNext() && num < 100) {
+            String[] chars = in.next().split(",");
+            int come = Integer.valueOf(chars[0]);
+            int leave = Integer.valueOf(chars[1]);
+
+            if(come >= leave) {
+                System.out.println("come time is larger than leave time");
+                continue;
+            }
+
+            if(-1 == come && -1 == leave) {
+                break;
+            }
+            if((come < 12)|| (come >= 20) || (leave <= 12)|| (leave > 20)) {
+                System.out.println("error time" +"[ " + come + " : "+ leave +" ]");
+                continue;
+            }
+            counts(come, leave);
+            num++;
+
+        }
+        if(100 == num ) {
+            System.out.println("customer has been 1000");
+        }
+        for(int i = 12; i < 20; i ++) {
+            System.out.println("[" + i + "," + (i+1)+"):" +map.get(i));
+        }
+
+    }
+
+    public static void counts (int come, int leave) {
+        for (int i = come; i < leave; i ++) {
+            map.put(i, map.get(i) + 1);
+        }
+    }
+}
